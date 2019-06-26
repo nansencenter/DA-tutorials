b6969da7dc6c3d064e2f36364497a3e4470739e9
patricknraanes <patrick.n.raanes@gmail.com>
2019-06-26 13:43:37 +0200

    Updated the setup

--- setup.py
+++ setup.py
@@ -7,24 +7,21 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 
-    # Basic meta
     name="DA-tutorials",
     version="1.0.0",
     author="Patrick N. Raanes",
     author_email="patrick.n.raanes@gmail.com",
     description="Tutorials on data assimilation (DA)",
 
-    python_requires='~=3.6', # (need >=3.6 for mpl 3.1)
+    python_requires='~=3.6',
     install_requires=[
       'DA-DAPPER>=0.9.2',
       'jupyter>=1.0.0',
       'Markdown>=2.6',
       ],
 
-    # Note: find_packages() only works on __init__ dirs.
     packages=setuptools.find_packages(),
 
-    # Detailed meta
     classifiers=[
         'Development Status :: 3 - Alpha',
 
@@ -40,13 +37,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/nansencenter/DA-tutorials",
     keywords='data-assimilation enkf kalman-filtering state-estimation particle-filter kalman bayesian-methods bayesian-filter chaos',
-    # project_urls={
-        # 'Documentation': 'https://packaging.python.org/tutorials/distributing-packages/',
-        # 'Funding': 'https://donate.pypi.org',
-        # 'Say Thanks!': 'http://saythanks.io/to/example',
-        # 'Source': 'https://github.com/pypa/sampleproject/',
-        # 'Tracker': 'https://github.com/pypa/sampleproject/issues',
-    # },
 )
 
 
