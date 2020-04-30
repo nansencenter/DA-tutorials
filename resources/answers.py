e97a5f2a7aefafd40e70ea0509488c57b1af7478
patricknraanes <patrick.n.raanes@gmail.com>
2020-04-30 15:42:46 +0200

    Fix mathjax on Colab

--- resources/answers.py
+++ resources/answers.py
@@ -7,9 +7,52 @@ from IPython.display import HTML, display
 #   For ex: _ in math sometimes gets replaced by <em>.
 #   Can be fixed by escaping, i.e. writing \_
 
+try:
+    import google.colab
+    is_colab = True
+except ImportError:
+    is_colab = False
+
+def setup_typeset():
+    """MathJax initialization for the current cell.
+
+    This installs and configures MathJax for the current output.
+
+    Necessary in Google Colab. Ref:
+    https://github.com/googlecolab/colabtools/issues/322
+    """
+    if not is_colab: return
+    display(HTML('''
+            <script src="https://www.gstatic.com/external_hosted/mathjax/latest/MathJax.js?config=TeX-AMS_HTML-full,Safe&delayStartupUntil=configured"></script>
+            <script>
+                (() => {
+                    const mathjax = window.MathJax;
+                    mathjax.Hub.Config({
+                    'tex2jax': {
+                        'inlineMath': [['$', '$'], ['\\(', '\\)']],
+                        'displayMath': [['$$', '$$'], ['\\[', '\\]']],
+                        'processEscapes': true,
+                        'processEnvironments': true,
+                        'skipTags': ['script', 'noscript', 'style', 'textarea', 'code'],
+                        'displayAlign': 'center',
+                    },
+                    'HTML-CSS': {
+                        'styles': {'.MathJax_Display': {'margin': 0}},
+                        'linebreaks': {'automatic': true},
+                        // Disable to prevent OTF font loading, which aren't part of our
+                        // distribution.
+                        'imageFont': null,
+                    },
+                    'messageStyle': 'none'
+                });
+                mathjax.Hub.Configured();
+            })();
+            </script>
+            '''))
+
 def formatted_display(TYPE,s,bg_color):
 
-  # Remove 1st linebreak
+    # Remove 1st linebreak
     s = s[1:]
 
     # Make bg style
@@ -21,7 +64,10 @@ def formatted_display(TYPE,s,bg_color):
     elif TYPE == "TXT" : s = '<pre><code>'+s+'</code></pre>'
 
     # Compose string
-    s = '<div style="'+bg+'padding:0.5em;">'+str(s)'</div>'
+    s = '<div style="'+bg+'padding:0.5em;">'+str(s)+'</div>'
+
+    # Fix Colab - MathJax incompatibility
+    setup_typeset()
 
     # Display
     display(HTML(s))
