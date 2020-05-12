04d4dfd745e3fea0308cae89c0a1e3240a9d8442
patricknraanes <patrick.n.raanes@gmail.com>
2020-05-12 19:03:56 +0200

    Consolidate macros and make update script

--- notebooks/resources/answers.py
+++ notebooks/resources/answers.py
@@ -1,6 +1,8 @@
 from markdown import markdown as md2html # better than markdown2 ?
 from IPython.display import HTML, display
 
+from .macros import macros
+
 # Gotchas:
 # - md2html rendering sometimes breaks
 #   because it has failed to parse the eqn properly.
@@ -89,56 +91,6 @@ def setup_typeset():
 answers = {}
 examples = {}
 
-macros=r'''%
-%MACRO DEFINITION
-\newcommand{\Reals}{\mathbb{R}}
-%
-\newcommand{\Expect}[0]{\mathbb{E}}
-\newcommand{\NormDist}{\mathcal{N}}
-%
-\newcommand{\mat}[1]{{\mathbf{{#1}}}} 
-%\newcommand{\mat}[1]{{\pmb{\mathsf{#1}}}}
-\newcommand{\bvec}[1]{{\mathbf{#1}}}
-%
-\newcommand{\trsign}{{\mathsf{T}}}
-\newcommand{\tr}{^{\trsign}}
-\newcommand{\tn}[1]{#1}
-%
-\newcommand{\I}[0]{\mat{I}}
-\newcommand{\K}[0]{\mat{K}}
-\newcommand{\bP}[0]{\mat{P}}
-\newcommand{\bH}[0]{\mat{H}}
-\newcommand{\bF}[0]{\mat{F}}
-\newcommand{\R}[0]{\mat{R}}
-\newcommand{\Q}[0]{\mat{Q}}
-\newcommand{\B}[0]{\mat{B}}
-\newcommand{\Ri}[0]{\R^{-1}}
-\newcommand{\Bi}[0]{\B^{-1}}
-\newcommand{\X}[0]{\mat{X}}
-\newcommand{\A}[0]{\mat{A}}
-\newcommand{\Y}[0]{\mat{Y}}
-\newcommand{\E}[0]{\mat{E}}
-\newcommand{\U}[0]{\mat{U}}
-\newcommand{\V}[0]{\mat{V}}
-%
-\newcommand{\x}[0]{\bvec{x}}
-\newcommand{\y}[0]{\bvec{y}}
-\newcommand{\q}[0]{\bvec{q}}
-\newcommand{\br}[0]{\bvec{r}}
-\newcommand{\bb}[0]{\bvec{b}}
-%
-\newcommand{\bx}[0]{\bvec{\bar{x}}}
-\newcommand{\barB}[0]{\mat{\bar{B}}}
-\newcommand{\barP}[0]{\mat{\bar{P}}}
-\newcommand{\barK}[0]{\mat{\bar{K}}}
-%
-\newcommand{\D}[0]{\mat{D}}
-\newcommand{\Dobs}[0]{\mat{D}\_{\text{obs}}}
-\newcommand{\Dmod}[0]{\mat{D}\_{\text{obs}}}
-%
-\newcommand{\ones}[0]{\bvec{1}}
-\newcommand{\AN}[0]{\big( \I\_N - \ones \ones\tr / N \big)}
-%'''
 
 ###########################################
 # Tut: DA & EnKF
