e447cbecdead61cd6f7df0b3d5ad39b5c57bafe6
patricknraanes <patrick.n.raanes@gmail.com>
2021-06-17 17:00:54 +0200

    Improve colab_bootstrap script

--- notebooks/resources/colab_bootstrap.sh
+++ notebooks/resources/colab_bootstrap.sh
@@ -1,30 +1,40 @@
 #!/usr/bin/env bash
 
-# Colab doesn't provide functionality for working with full packages/repos, including
+# Colab doesn't provide functionality for
+# working with full packages/repos, including
 # - defining python environments (e.g. requirements.txt)
 # - pre-loading data/scripts other than what's in the notebook.
-# We therefore make this script for bootstrapping the notebooks by cloning the full repo.
-# It should be run in a Colab notebook using:
-# URL="https://raw.githubusercontent.com/nansencenter/DA-tutorials/master/notebooks/resources/colab_bootstrap.sh"
-#!wget -qO- $URL | bash -s -- --debug
+# We therefore make this script for bootstrapping the notebooks
+# by cloning the full repo.
 
 
-setup () {
+# Install requirements
+main () {
     set -e
+
+    # Clear cache
+    rm -rf /root/.cache
+
+    # Download repo
     URL=https://github.com/nansencenter/DA-tutorials.git
     if [[ ! -d REPO ]]; then git clone --depth=1 $URL REPO; fi
+
+    # Install requirements
     pip install -r REPO/requirements.txt
+
+    # Put repo contents in PWD
     cp -r REPO/notebooks/resources ./
     cp REPO/notebooks/dpr_config.ini ./
 }
 
 # Only run if we're on colab
 if python -c "import colab"; then
-    # Quiet execution
     if echo $@ | grep -- '--debug' > /dev/null ; then
-        setup
+        # Verbose (regular output)
+        main
     else
-        setup > /dev/null 2>&1
+        # Quiet execution
+        main > /dev/null 2>&1
     fi
     echo "Initialization for Colab done."
 fi
