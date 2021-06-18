f3aaa71e2d7b5d061b5693409e28edb3d7259b15
patricknraanes <patrick.n.raanes@gmail.com>
2021-06-18 12:08:17 +0200

    Improve colab_bootstrap (as for HM-tutorials)

--- notebooks/resources/colab_bootstrap.sh
+++ notebooks/resources/colab_bootstrap.sh
@@ -7,13 +7,27 @@
 # We therefore make this script for bootstrapping the notebooks
 # by cloning the full repo.
 
+#############
+#  WARNING  #
+#############
+# This will ALWAYS give you the current master version of the repo,
+# something which can be a source of confusion, e.g.:
+# - When bug hunting by checking out previous commits.
+# - When working on Colab branch, and changing something other than
+#   the notebook or script. This change won't be used on Colab
+#   until it's merged into master. Of course, we could point the
+#   below download to the Colab branch, but that won't fix the previous item,
+#   and also assumes that we make the choice of maintaining a separate branch
+#   for Colab (instead of using the check `import google.colab`, or telling
+#   user to insert the appropriate code when running on colab).
+
 
 # Install requirements
 main () {
     set -e
 
-    # Clear cache
-    rm -rf /root/.cache
+    # Clear any existing REPO for a fresh git clone
+    rm -rf REPO
 
     # Download repo
     URL=https://github.com/nansencenter/DA-tutorials.git
@@ -29,12 +43,14 @@ main () {
 
 # Only run if we're on colab
 if python -c "import colab"; then
-    if echo $@ | grep -- '--debug' > /dev/null ; then
-        # Verbose (regular output)
+
+    # Use `bash -s -- --debug` to get verbose output
+    if echo $@ | grep -E -- '(--debug|-v)' > /dev/null ; then
         main
     else
-        # Quiet execution
+        # Quiet
         main > /dev/null 2>&1
     fi
+
     echo "Initialization for Colab done."
 fi
