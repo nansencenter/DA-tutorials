b8b30535ea67e00ccbddd72f1ce35ba735a5160c
patnr <patrick.n.raanes@gmail.com>
2022-09-22 12:47:17 +0200

    Fix typo

--- notebooks/resources/answers.py
+++ notebooks/resources/answers.py
@@ -298,8 +298,8 @@ $$ \begin{align}
 p\, (y_1, \ldots, y_K \;|\; a)
 &= \prod_{k=1}^K \, p\, (y_k \;|\; a) \tag{each obs. is indep. of others, knowing $a$.} \\\
 &= \prod_k \, \mathcal{N}(y_k \mid a k,R) \tag{inserted eqn. (3) and then (1).} \\\
-&= \prod_k \,  (2 \pi R)^{-1/2} e^{-(x - a k)^2/2 R} \tag{inserted eqn. (G1) from T2.} \\\
-&= c \exp\Big(\frac{-1}{2 R}\sum_k (x - a k)^2\Big) \\\
+&= \prod_k \,  (2 \pi R)^{-1/2} e^{-(y_k - a k)^2/2 R} \tag{inserted eqn. (G1) from T2.} \\\
+&= c \exp\Big(\frac{-1}{2 R}\sum_k (y_k - a k)^2\Big) \\\
 \end{align} $$
 Taking the logarithm is a monotonic transformation, so it does not change the location of the maximum location.
 Neither does diving by $c$. Multiplying by $-2 R$ means that the maximum becomes the minimum.
