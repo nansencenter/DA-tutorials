8e1d85b412c12bf668375347d7d7710ad2c174ca
patnr <patrick.n.raanes@gmail.com>
2023-03-28 15:28:44 +0200

    Fix colab

--- notebooks/resources/colab_bootstrap.sh
+++ notebooks/resources/colab_bootstrap.sh
@@ -42,7 +42,7 @@ main () {
 }
 
 # Only run if we're on colab
-if python -c "import colab"; then
+if python -c "import google.colab"; then
 
     # Use `bash -s -- --debug` to get verbose output
     if echo $@ | grep -E -- '(--debug|-v)' > /dev/null ; then
