0e4ab857718eabb780827fd46a07384ea2d3453c
patnr <patrick.n.raanes@gmail.com>
2023-08-16 08:27:26 +0200

    Enable using single branch

--- notebooks/resources/colab_bootstrap.sh
+++ notebooks/resources/colab_bootstrap.sh
@@ -1,27 +1,9 @@
 #!/usr/bin/env bash
 
-# This script provides functionality for working with full repos on Colab
-# - Installing packages/dependencies from requirements.txt
-# - Loading data/scripts other than what's in the notebook.
-
-# Warning: This will ALWAYS give you the **'master'** branch of the repo.
-# Reasons:
-# - Script gets run from a notebook which doesn't know its branch.
-# - If script always checks out 'Colab' branch,
-#   that might be surprising if on a 'Colab-dev' branch.
-# - Maybe in future there won't be a 'Colab' branch (we can use instead an
-#   `import google.colab` guard, or telling user to manually run script).
-
-
-#############
-#  WARNING  #
-#############
-# - When bug hunting by checking out previous commits.
-# - When working on Colab branch, and changing something other than
-#   the notebook or script. This change won't be used on Colab
-#   until it's merged into master. Of course, we could point the
-#   below download to the Colab branch, but that won't fix the previous item,
-
+# Colab doesn't provide
+# - Auto-installing requirements.txt
+# - Pre-loading data/modules (aside from the notebook itself)
+# This script takes care of the above by cloning the full (shallow) repo.
 
 # Install requirements
 main () {
@@ -34,6 +16,9 @@ main () {
     URL=https://github.com/nansencenter/DA-tutorials.git
     if [[ ! -d REPO ]]; then git clone --depth=1 $URL REPO; fi
 
+    # https://pythonspeed.com/articles/upgrade-pip/
+    pip install --upgrade pip
+
     # Install requirements
     pip install -r REPO/requirements.txt
 
@@ -43,7 +28,7 @@ main () {
 }
 
 # Only run if we're on colab
-if python -c "import google.colab"; then
+if python -c "import google.colab" 2>/dev/null; then
 
     # Use `bash -s -- --debug` to get verbose output
     if echo $@ | grep -E -- '(--debug|-v)' > /dev/null ; then
@@ -54,4 +39,6 @@ if python -c "import google.colab"; then
     fi
 
     echo "Initialization for Colab done."
+else
+    echo "Not running on Colab => Didn't do anything."
 fi
